17ace0d4dd05ff0b70e42e52b0498ddaa042e50d
Lukas Martinez <lukasmartinezc@gmail.com>
2025-08-30 17:06:30 -0400

    Se crea el proyecto express básico

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import colors from 'colors'
+import server from './server.js'
+
+const PORT = process.env.PORT ?? 3000
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`.cyan)
+})
